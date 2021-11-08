6b31d198ede4e91c3087910a1fbf5fc2cf624acd
narimiran <narimiran@disroot.org>
2021-11-08 18:21:47 +0100

    enable `chronos`, disable `nimpy`

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -17,8 +17,7 @@ pkg "c2nim", false, "nim c testsuite/tester.nim"
 pkg "cascade"
 pkg "chroma"
 pkg "chronicles", true, "nim c -o:chr -r chronicles.nim"
-# disable until my chronos fix was merged
-#pkg "chronos", true
+pkg "chronos", true
 pkg "cligen", false, "nim c -o:cligenn -r cligen.nim"
 pkg "coco", true
 pkg "combparser"
@@ -57,7 +56,7 @@ pkg "nimfp", true, "nim c -o:nfp -r src/fp.nim"
 # pkg "nimlsp", true
 # pkg "nimly", true
 # pkg "nimongo", true, "nimble test_ci"
-pkg "nimpy", false, "nim c -r tests/nimfrompy.nim"
+# pkg "nimpy", false, "nim c -r tests/nimfrompy.nim"
 # pkg "nimquery"
 pkg "nimsl", true
 pkg "nimsvg"
